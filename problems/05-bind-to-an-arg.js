14529a3c78cc034deefecf04162294dcda13d225
mathi <mathieu.t.evans@gmail.com>
2022-06-02 14:08:29 -0700

    fancy

--- problems/05-bind-to-an-arg.js
+++ problems/05-bind-to-an-arg.js
@@ -1,6 +1,14 @@
 function bindToAnArg(func, arg) {
-  // Your code here
+  // return func.bind(null, arg)
+  return func.bind(thing, arg)
 }
+
+// function iSpy(thing) {
+//   return "I spy a " + thing;
+// }
+
+// let spyTree = bindToAnArg(iSpy, "tree");
+// let spyCar = bindToAnArg(iSpy, "car");
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = bindToAnArg;
--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,4 +1,21 @@
-// Your code here
+const Calculator = require('./02-calculator')
+
+class FancyCalculator extends Calculator {
+	constructor(total = 0) {
+		super(total)
+		this.total = total
+	}
+	setTotal(newTotal) {
+		return this.total = newTotal
+	}
+	modulo(num) {
+		this.total = this.total % num
+		return this.total
+	}
+	squared() {
+		return this.total ** 2
+	}
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
