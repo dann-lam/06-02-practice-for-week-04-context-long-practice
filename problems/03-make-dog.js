483356cb5640d84c0762d9c99fc5455c55edfbeb
Daniel Lam <Dann.lam@gmail.com>
2022-06-02 13:11:51 -0700

    Third!

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,4 +1,22 @@
-// Your code here
+class Dog {
+	constructor(name){
+		this.name = name
+
+	}
+	static makeJet(){
+		const Jet = new Dog('Jet');
+		// Jet.name = "Jet"
+		return Jet
+	}
+	changeName(newName){
+		// this.name = newName
+		// return this.name
+		return this.name = newName;
+	}
+	speak(word){
+		return `${this.name} says ${word}`
+	}
+}
 
 
 /*****************************************************************************/
