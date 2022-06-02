09883d5ddb63199843b7cdd5f789c3f8abf130c3
Daniel Lam <Dann.lam@gmail.com>
2022-06-02 12:43:12 -0700

    First commit!

---
+++ problems/01-car-drive.js
@@ -0,0 +1,19 @@
+class Car{
+	constructor(speed){
+	this.speed = speed
+	}
+	drive(speed){
+		return speed
+	}
+
+}
+
+
+/*****************************************************************************/
+/***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
+
+try {
+	module.exports = Car;
+} catch {
+	module.exports = null;
+}
