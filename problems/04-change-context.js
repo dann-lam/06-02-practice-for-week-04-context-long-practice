7bc6b07030c95d293552e3aab9fcae66d9415155
Daniel Lam <Dann.lam@gmail.com>
2022-06-02 13:22:56 -0700

    Fourth commit!

--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,6 +1,24 @@
 function changeContext(func, obj) {
-  // Your code here
+  // func.call(obj)
+
+  // return above if you want to invoke it immediately
+  let name = func.bind(obj);
+  return name();
+  //Bind variant being invoked to get name.
 }
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+function extractName() {
+  return this.name;
+}
+
+const kristen = new Person("Kristen");
+console.log(changeContext(extractName, kristen));
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
